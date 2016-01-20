792753ef3c88fdb69025284facfb0c2a0b293e7a
Ghithub <gnewsgroup@gmail.com>
2016-01-20 17:45:44 -0500

    a bunch of starter files

---
+++ Scripts/tutorial2.js
@@ -0,0 +1,20 @@
+var CommentList = React.createClass( {
+    render: function(){
+        return (
+            <div className="commentList">
+                Hello, I am a CommentList.
+            </div>
+        );
+    }
+});
+
+var CommentForm = React.createClass({
+    render: function(){
+        return (
+            <div className="commentForm">
+               Hello, I am a CommentForm.
+            </div>
+        );
+    }
+    
+});
